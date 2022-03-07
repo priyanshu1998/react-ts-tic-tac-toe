a4913eb16dfbc4bfaeee159c2e086f23201f48ab
priyanshu1998 <priyanshutirkey1998@gmail.com>
2022-03-07 21:37:42 +0530

    implemented result component

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,6 @@ class TicTacToe extends Component<{}, GameState>{
 
 
   onClickEvent = (index: number) => {
-    
     let positions = [...this.state.positions]
     positions[index] = this.state.player
     this.setState({ positions: positions })
@@ -36,18 +35,23 @@ class TicTacToe extends Component<{}, GameState>{
   }
 
   render() {
+    let winner = Result.detectWinner(this.state.positions)
     return (
-      <div className='grid'>
-        <Square position={0} value={this.state.positions[0]} cb={this.onClickEvent} />
-        <Square position={1} value={this.state.positions[1]} cb={this.onClickEvent} />
-        <Square position={2} value={this.state.positions[2]} cb={this.onClickEvent} />
-        <Square position={3} value={this.state.positions[3]} cb={this.onClickEvent} />
-        <Square position={4} value={this.state.positions[4]} cb={this.onClickEvent} />
-        <Square position={5} value={this.state.positions[5]} cb={this.onClickEvent} />
-        <Square position={6} value={this.state.positions[6]} cb={this.onClickEvent} />
-        <Square position={7} value={this.state.positions[7]} cb={this.onClickEvent} />
-        <Square position={8} value={this.state.positions[8]} cb={this.onClickEvent} />
-
+      <div>
+        <div className='grid'>
+          <Square position={0} value={this.state.positions[0]} cb={this.onClickEvent} />
+          <Square position={1} value={this.state.positions[1]} cb={this.onClickEvent} />
+          <Square position={2} value={this.state.positions[2]} cb={this.onClickEvent} />
+          <Square position={3} value={this.state.positions[3]} cb={this.onClickEvent} />
+          <Square position={4} value={this.state.positions[4]} cb={this.onClickEvent} />
+          <Square position={5} value={this.state.positions[5]} cb={this.onClickEvent} />
+          <Square position={6} value={this.state.positions[6]} cb={this.onClickEvent} />
+          <Square position={7} value={this.state.positions[7]} cb={this.onClickEvent} />
+          <Square position={8} value={this.state.positions[8]} cb={this.onClickEvent} />
+        </div>
+        <div>
+          {(winner == "CIRCLE" || winner == "CROSS") && <Result winner={winner} />}
+        </div>
       </div>
     );
   }
@@ -92,10 +96,56 @@ class Cross extends Component {
   }
 }
 
-class Result extends Component {
+
+
+
+class Result extends Component<{ winner: string }> {
+  static detectWinner(grid: string[]) {
+    for (let i = 0; i < 3; i++) {
+      if (grid[3 * i] === grid[3 * i + 1] && grid[3 * i + 1] === grid[3 * i + 2]) {
+        if (grid[3 * i] === CROSS) {
+          return CROSS
+        } else if (grid[3 * i] === CIRCLE) {
+          return CIRCLE
+        }
+      }
+    }
+
+    for (let j = 0; j < 3; j++) {
+      if (grid[3 * 0 + j] === grid[3 * 1 + j] && grid[3 * 1 + j] === grid[3 * 2 + j]) {
+        if (grid[3 * 0 + j] === CROSS) {
+          return CROSS
+        } else if (grid[3 * 0 + j] === CIRCLE) {
+          return CIRCLE
+        }
+      }
+    }
+
+    if (grid[0] === grid[4] && grid[4] === grid[8]) {
+      if (grid[0] === CROSS) {
+        return CROSS
+      } else if (grid[0] === CIRCLE) {
+        return CIRCLE
+      }
+    }
+
+    if (grid[2] === grid[4] && grid[4] === grid[6]) {
+      if (grid[2] === CROSS) {
+        return CROSS
+      } else if (grid[0] === CIRCLE) {
+        return CIRCLE
+      }
+    }
+
+
+    if (grid.every((v) => v != EMPTY)) return "TIE"
+    return "NO RESULT"
+  }
+
   render() {
+    const { winner } = this.props;
     return (
-      <div>Result</div>
+      <div>{winner} won the game. </div>
     )
   }
 }
