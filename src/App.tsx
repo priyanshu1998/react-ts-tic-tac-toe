ad280b460b0fe0f5d3722742e573c13b562a3889
priyanshu1998 <priyanshutirkey1998@gmail.com>
2022-03-07 20:52:23 +0530

    implemented on click event

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,89 @@
 import React, { Component } from 'react';
-import  './index.css';
+import './index.css';
 
 
-interface GameState{
+interface GameState {
   player: string
-  positions: Array<string>
+  positions: Array<string>,
 }
 
 const EMPTY = "EMPTY";
 const CIRCLE = "CIRCLE";
 const CROSS = "CROSS";
 
-class TicTacToe extends Component<{},GameState>{
-  constructor(props: GameState){
+class TicTacToe extends Component<{}, GameState>{
+  constructor(props: GameState) {
     super(props);
     this.state = {
-      player: "",
-      positions: 
-      [ EMPTY, EMPTY, CIRCLE,
-        EMPTY, CROSS, EMPTY,
-        CROSS, EMPTY, EMPTY,]
+      player: "CROSS",
+      positions: [
+        EMPTY, EMPTY, EMPTY,
+        EMPTY, EMPTY, EMPTY,
+        EMPTY, EMPTY, EMPTY,
+      ],
     }
   }
-  render(){
+
+
+  onClickEvent = (index: number) => {
+    
+    let positions = [...this.state.positions]
+    positions[index] = this.state.player
+    this.setState({ positions: positions })
+
+    this.state.player === CIRCLE && this.setState({ player: CROSS })
+    this.state.player === CROSS && this.setState({ player: CIRCLE })
+  }
+
+  render() {
     return (
       <div className='grid'>
-        <Square position={0} value={this.state.positions[0]}/>
-        <Square position={1} value={this.state.positions[1]}/>
-        <Square position={2} value={this.state.positions[2]}/>
-        <Square position={3} value={this.state.positions[3]}/>
-        <Square position={4} value={this.state.positions[4]}/>
-        <Square position={5} value={this.state.positions[5]}/>
-        <Square position={6} value={this.state.positions[6]}/>
-        <Square position={7} value={this.state.positions[7]}/>
-        <Square position={8} value={this.state.positions[8]}/>
+        <Square position={0} value={this.state.positions[0]} cb={this.onClickEvent} />
+        <Square position={1} value={this.state.positions[1]} cb={this.onClickEvent} />
+        <Square position={2} value={this.state.positions[2]} cb={this.onClickEvent} />
+        <Square position={3} value={this.state.positions[3]} cb={this.onClickEvent} />
+        <Square position={4} value={this.state.positions[4]} cb={this.onClickEvent} />
+        <Square position={5} value={this.state.positions[5]} cb={this.onClickEvent} />
+        <Square position={6} value={this.state.positions[6]} cb={this.onClickEvent} />
+        <Square position={7} value={this.state.positions[7]} cb={this.onClickEvent} />
+        <Square position={8} value={this.state.positions[8]} cb={this.onClickEvent} />
 
       </div>
     );
   }
 }
 
-interface ISquare{
+interface ISquare {
   position: number
   value: string
+  cb: Function
 }
 
 class Square extends Component<ISquare>{
-  render(){
-    const {position, value} = this.props
+  render() {
+    const { position, value, cb } = this.props
     return (
-      <div className='square'>
-      {value === CIRCLE && <Circle/>}
-      {value === CROSS && <Cross/>}
+      <div className='square' onClick={() => cb(position)}>
+        {value === CIRCLE && <Circle />}
+        {value === CROSS && <Cross />}
       </div>
     )
   }
 }
 
-class Circle extends Component{
-  render(){
+class Circle extends Component {
+  render() {
     return (
       <svg width={100} height={100} viewBox="-50 -50 100 100" className='circle'>
-        <circle cx={0} cy={0} r={40}/>  
+        <circle cx={0} cy={0} r={40} />
       </svg>
     )
   }
 }
 
-class Cross extends Component{
-  render(){
-    return(
+class Cross extends Component {
+  render() {
+    return (
       <svg width={100} height={100} viewBox="-50 -50 100 100" className='cross'>
         <line x1={-40} y1={-40} x2={40} y2={40}></line>
         <line x1={-40} y1={40} x2={40} y2={-40}></line>
@@ -78,9 +92,9 @@ class Cross extends Component{
   }
 }
 
-class Result extends Component{
-  render(){
-    return(
+class Result extends Component {
+  render() {
+    return (
       <div>Result</div>
     )
   }
